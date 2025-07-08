eab8b0940d3e86ad1da260b19bc6168225df0225
MadanMohan-M <madanmohan22.m@gmail.com>
2025-07-08 15:30:41 +0530

    Fix: force install of tesseract and poppler on Render

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y poppler-utils tesseract-ocr
+# Grant root permissions
+echo "Installing system dependencies..."
+apt-get update -y && apt-get install -y tesseract-ocr poppler-utils
 
+# Start the Streamlit app
 streamlit run app.py --server.port=$PORT
